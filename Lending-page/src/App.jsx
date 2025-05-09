4b13467517298ed71bb3e49b45c99c8dadec9da4
jashvanth370 <jashjazz11@gmail.com>
2025-05-09 08:26:04 +0530

    Final Commit

--- Lending-page/src/App.jsx
+++ Lending-page/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Home from './components/Home'
+import Testimonial from './components/Teatimonial'
+import Work from './components/Work'
 
 function App() {
   return (
     <div className='App'>
       <Home />
       <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
       
     
